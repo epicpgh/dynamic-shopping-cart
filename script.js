c01245c9ccf87f3c6443981a902aeefca56b23f6
Evan Miller <ewmst5@gmail.com>
2025-05-15 22:03:31 -0400

    functional javascript implementation.  Will return at some point to style page, but I consider this a tiny victory.

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ let totalPrice = 0;
 
 // Function to update the total price
 function updateTotalPrice(amount) {
+  if (isNaN(amount)){
+    alert('Invalid Input:  You Gotta Use This Number Format, Chief: 00.00');
+    return;
+  }
+
+  
   totalPrice += amount;
   totalPriceSpan.textContent = totalPrice.toFixed(2);
 }
@@ -23,7 +29,21 @@ addProductButton.addEventListener("click", () => {
 //Offers validation feedback if invalid product is entered
 
 
+
+
+
+
 function addItem() {
+
+  const productPrice = productPriceInput.value;
+  const productName =productNameInput.value;
+
+  if (isNaN(productPrice) || !/^\d+\.\d{2}$/.test(productPrice)) {
+    alert('You Gotta Use This Number Format, Friend: 00.00. Numbers and decimals only.');
+    return;
+  }
+
+
   const addProduct = document.createElement("li");
   addProduct.dataset.price = productPriceInput.value;
   addProduct.innerHTML = `<span>${productNameInput.value} - ${productPriceInput.value}</span> <button class="remove-product">Remove</button>`;
